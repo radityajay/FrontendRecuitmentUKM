52567964c3ac93d72e7697b0a492c222a3ebe09e
radityajay <radityajayantara76@gmail.com>
2022-01-17 02:15:10 +0800

    Update final router

--- src/router.js
+++ src/router.js
@@ -6,6 +6,12 @@ import Futsal from "./views/futsal/Futsal.vue";
 import Dance from "./views/dance/Dance.vue";
 import Gmpa from "./views/gmpa/Gmpa.vue";
 import Ec from "./views/ec/Ec.vue";
+import Musik from "./views/musik/Musik.vue";
+import Hipmi from "./views/hipmi/Hipmi.vue";
+import Im from "./views/im/Im.vue";
+import Pmc from "./views/pmc/Pmc.vue";
+import Gamedev from "./views/gamedev/Gamedev.vue";
+import Primdev from "./views/primdev/Primdev.vue";
 import Esprime from "./views/esprime/Esprime.vue";
 import UKM from "./views/ukm/Ukm.vue";
 import Event from "./views/events/Event.vue";
@@ -15,10 +21,16 @@ import EditEvent from "./views/events/Edit.vue";
 import DelUkm from "./views/ukm/Delete.vue";
 import DelFutsal from "./views/futsal/Delete.vue";
 import DelEc from "./views/ec/Delete.vue";
+import DelMusik from "./views/musik/Delete.vue";
+import DelIm from "./views/im/Delete.vue";
+import DelGamedev from "./views/gamedev/Delete.vue";
 import DelGmpa from "./views/gmpa/Delete.vue";
 import DelDance from "./views/dance/Delete.vue";
 import DelEsprime from "./views/esprime/Delete.vue";
 import DelEvent from "./views/events/Delete.vue";
+import DelPmc from "./views/pmc/Delete.vue";
+import DelHipmi from "./views/hipmi/Delete.vue";
+import DelPrimdev from "./views/primdev/Delete.vue";
 import AddUkm from "./views/ukm/Add.vue";
 import AddEvent from "./views/events/Add.vue";
 // lazy-loaded
@@ -44,6 +56,30 @@ const routes = [
   {
     path: "/ec",
     component: Ec,
+  },
+  {
+    path: "/musik",
+    component: Musik,
+  },
+  {
+    path: "/hipmi",
+    component: Hipmi,
+  },
+  {
+    path: "/im",
+    component: Im,
+  },
+  {
+    path: "/pmc",
+    component: Pmc,
+  },
+  {
+    path: "/gamedev",
+    component: Gamedev,
+  },
+  {
+    path: "/primdev",
+    component: Primdev,
   },
   {
     path: "/gmpa",
@@ -76,6 +112,30 @@ const routes = [
   {
     path: "/delec/:id",
     component: DelEc,
+  },
+  {
+    path: "/delmusic/:id",
+    component: DelMusik,
+  },
+  {
+    path: "/delhipmi/:id",
+    component: DelHipmi,
+  },
+  {
+    path: "/delim/:id",
+    component: DelIm,
+  },
+  {
+    path: "/delpmc/:id",
+    component: DelPmc,
+  },
+  {
+    path: "/delgamedev/:id",
+    component: DelGamedev,
+  },
+  {
+    path: "/delprimdev/:id",
+    component: DelPrimdev,
   },
   {
     path: "/delgmpa/:id",
